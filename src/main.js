20cfa3f2904d227790e8e3408fdc88ce888972c0
Shkurinskaya Irina <ShkurinskayaIrina@gmail.com>
2021-07-29 14:49:13 +0300

    Начинаем программировать

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
+// import { createSiteMenuTemplate } from './view/menu.js';
+import { createSiteMenuTemplate } from './view/menu.js';
+import { createFilterMenuTemplate } from './view/filter.js';
+import { createSiteSortTemplate } from './view/sort.js';
+import { createTripEventsTemplate } from './view/event.js';
 
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteMenuElement = document.querySelector('.trip-controls__navigation');
+render(siteMenuElement, createSiteMenuTemplate(), 'beforeend');
+
+const siteFiltersElement = document.querySelector('.trip-controls__filters');
+render(siteFiltersElement, createFilterMenuTemplate(), 'beforeend');
+
+const siteMainElement = document.querySelector('main').querySelector('.trip-events');
+render(siteMainElement, createSiteSortTemplate(), 'beforeend');
+render(siteMainElement, createTripEventsTemplate(), 'beforeend');
---
+++ src/view/menu.js
@@ -0,0 +1,11 @@
+const siteMenuTemplate = `
+<nav class="trip-controls__trip-tabs  trip-tabs">
+<a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+<a class="trip-tabs__btn" href="#">Stats</a>
+</nav>`;
+
+const createSiteMenuTemplate = function () {
+  return siteMenuTemplate;
+};
+
+export {createSiteMenuTemplate};
