a7e4b9f1736c564cb312f9762d17fcdcbd65c95b
Shkurinskaya Irina <ShkurinskayaIrina@gmail.com>
2021-08-07 02:40:16 +0300

    2.16. Шаблонизируй это

---
+++ src/view/data.js
@@ -0,0 +1,161 @@
+// Everthing – 'Click New Event to create your first point' - В случае отсутствия точек маршрута вместо списка отображается
+// * Past — 'There are no past events now';
+// * Future — 'There are no future events now'.
+
+const eventTypes= [
+  'taxi',
+  'bus',
+  'train',
+  'ship',
+  'drive',
+  'flight',
+  'check-in',
+  'sightseeing',
+  'restaurant',
+];
+
+const destinationСities = [
+  'Chamonix',
+  'Amsterdam',
+  'Geneva',
+  'London',
+  'Vyshny Volochek',
+  'Rostov-on-Don',
+  'Moscow'];
+
+const additionalOptions = [
+  {'type': 'taxi',
+    'offers': [
+      {
+        'title' : 'Upgrade to a business class',
+        'price' : 120,
+      }, {
+        'title' : 'Choose the radio station',
+        'price' : 60,
+      },
+    ],
+  },
+  {
+    'type': 'bus',
+    'offers': [
+      {
+        'title' : 'Infotainment system',
+        'price' : 50,
+      }, {
+        'title' : 'Order meal',
+        'price' : 100,
+      }, {
+        'title' : 'Choose seats',
+        'price' : 190,
+      },
+    ],
+  },
+  {
+    'type' : 'train',
+    'offers': [
+      {
+        'title' :'Book a taxi at the arrival point',
+        'price' : 110,
+      }, {
+        'title' : 'Order a breakfast',
+        'price' : 80,
+      }, {
+        'title' : 'Wake up at a certain time',
+        'price' : 140,
+      }, {
+        'title' : 'Switch to comfort class',
+        'price' : 100,
+      },
+    ],
+  },
+  {
+    'type' : 'ship',
+    'offers' : [
+      {
+        'title' : 'Choose meal',
+        'price' : 130,
+      }, {
+        'title' : 'Upgrade to comfort class',
+        'price' : 170,
+      }, {
+        'title' : 'Upgrade to business class',
+        'price' : 150,
+      }, {
+        'title' : 'Add luggage',
+        'price' : 100,
+      }, {
+        'title' : 'Business lounge',
+        'price' : 40,
+      },
+    ],
+  },
+  {
+    'type': 'drive',
+    'offers' : [
+      {
+        'title' : 'Choose comfort class',
+        'price' : 110,
+      }, {
+        'title' : 'Choose business class',
+        'price' : 180,
+      },
+    ],
+  },
+  {
+    'type': 'flight',
+    'offers': [
+      {
+        'title' : 'Choose meal',
+        'price': 120,
+      }, {
+        'title' :'Choose seats',
+        'price': 90,
+      }, {
+        'title' :'Upgrade to comfort class',
+        'price': 120,
+      }, {
+        'title' :'Upgrade to business class',
+        'price': 120,
+      }, {
+        'title' :'Business lounge',
+        'price' : 160,
+      },
+    ],
+  },
+  {
+    'type': 'restaurant',
+    'offers': [
+      {
+        'title' :'Choose live music',
+        'price' : 150,
+      }, {
+        'title' :'Choose VIP area',
+        'price' : 70,
+      },
+    ],
+  },
+  {
+    'type': 'check-in',
+    'offers': [],
+  },
+  {
+    'type': 'sightseeing',
+    'offers': [],
+  },
+];
+
+const destinationDescriptions =[
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Cras aliquet varius magna, non porta ligula feugiat eget.',
+  'Fusce tristique felis at fermentum pharetra.',
+  'Aliquam id orci ut lectus varius viverra.',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
+  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
+  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
+  'Sed sed nisi sed augue convallis suscipit in sed felis.',
+  'Aliquam erat volutpat.',
+  'Nunc fermentum tortor ac porta dapibus.',
+  'In rutrum ac purus sit amet tempus.',
+];
+
+export {eventTypes, destinationСities, destinationDescriptions, additionalOptions};
